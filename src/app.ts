0f18ddbc10753441f8ca2e6e1535f678ea74cff5
Hritik Kumar Sinha <hritik.span@gmail.com>
2026-02-24 22:34:16 +0530

    feat: register cors and helmet plugins

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
+import helmet from "@fastify/helmet";
 
 import type { FastifyRequest, FastifyReply } from "fastify";
 
@@ -8,6 +10,11 @@ const app = fastify({
     logger: true,
 });
 
+// registering required plugins
+app.register(cors);
+app.register(helmet);
+
+// all routes
 app.get("/health", async (_request: FastifyRequest, reply: FastifyReply) => {
     return reply.status(200).send({ statusCode: 200, message: "Server is up and running" });
 });
