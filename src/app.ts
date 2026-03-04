1b07da915292c8f17d262eeb903cca2c2038a127
Hritik Kumar Sinha <hritik.span@gmail.com>
2026-03-04 16:20:31 +0530

    feat: setup dev tools including eslint, husky, prettier, and test-coverage

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
-import fastify from "fastify";
-import cors from "@fastify/cors";
-import helmet from "@fastify/helmet";
-
-import type { FastifyRequest, FastifyReply } from "fastify";
-
+import cors from '@fastify/cors';
+import helmet from '@fastify/helmet';
+import fastify, { type FastifyReply, type FastifyRequest } from 'fastify';
 
 // creating fastify instance
 const app = fastify({
-    logger: true,
+    logger: false,
 });
 
 // registering required plugins
@@ -15,8 +12,10 @@ app.register(cors);
 app.register(helmet);
 
 // all routes
-app.get("/health", async (_request: FastifyRequest, reply: FastifyReply) => {
-    return reply.status(200).send({ statusCode: 200, message: "Server is up and running" });
+app.get('/health', async (_request: FastifyRequest, reply: FastifyReply) => {
+    return reply
+        .status(200)
+        .send({ statusCode: 200, message: 'Server is up and running' });
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import app from "./app.js";
-
+import app from './app.js';
 
 async function start() {
     await app.listen({ port: 6969 });
-    console.log("Server started at http://localhost:6969")
+
+    // eslint-disable-next-line no-console
+    console.log('Server started at http://localhost:6969');
 }
 
-start();
+await start();
